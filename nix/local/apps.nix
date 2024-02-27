38b4e67b9aa8cdfe54c6db5ea1c400967dfe22b2
Sergey Bubnov <omgbebebe@gmail.com>
2024-02-28 00:42:14 +0400

    initial vector-input.toml config

--- nix/local/apps.nix
+++ nix/local/apps.nix
@@ -10,7 +10,7 @@ in {
   vector-input = inputs.std.lib.ops.mkOperable {
     package = packages.vector.overrideAttrs (f: p: { pname = p.pname + "-input";});
     runtimeScript = ''
-      exec ${packages.vector}/bin/vector --config vector-input.toml
+      exec ${packages.vector}/bin/vector --config ./nix/local/configs/vector-input.toml
     '';
 
     meta.description = "A vector instance to parse an user input";
