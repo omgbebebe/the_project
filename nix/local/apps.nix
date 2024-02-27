eec3e226d8558fc46b86647114fce41cbffbbd22
Sergey Bubnov <omgbebebe@gmail.com>
2024-02-28 01:07:59 +0400

    use entr to watch for vector config changes

--- nix/local/apps.nix
+++ nix/local/apps.nix
@@ -10,7 +10,8 @@ in {
   vector-input = inputs.std.lib.ops.mkOperable {
     package = packages.vector.overrideAttrs (f: p: { pname = p.pname + "-input";});
     runtimeScript = ''
-      exec ${packages.vector}/bin/vector --config ./nix/local/configs/vector-input.toml
+      exec echo ./nix/local/configs/vector-input.toml \
+      | ${inputs.nixpkgs.entr}/bin/entr -r ${packages.vector}/bin/vector --config ./nix/local/configs/vector-input.toml
     '';
 
     meta.description = "A vector instance to parse an user input";
