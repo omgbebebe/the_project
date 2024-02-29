460eb4fe57e7a085c5de1207a27d0b69a46703a6
Sergey Bubnov <omgbebebe@gmail.com>
2024-02-29 12:55:35 +0400

    use a name param of mkOperable instead of overriding package name

--- nix/local/apps.nix
+++ nix/local/apps.nix
@@ -12,23 +12,21 @@ let
   };
 in {
   vector-input = inputs.std.lib.ops.mkOperable {
-    package = packages.vector.overrideAttrs (f: p: { pname = p.pname + "-input";});
+    package = packages.vector;
+    name = "vector-input";
     runtimeScript = ''
       exec echo ./nix/local/configs/vector-input.toml \
       | ${inputs.nixpkgs.entr}/bin/entr -r ${packages.vector}/bin/vector --config ./nix/local/configs/vector-input.toml
     '';
-
     meta.description = "A vector instance to parse an user input";
   };
   vector-output = inputs.std.lib.ops.mkOperable {
-    package = packages.vector.overrideAttrs (f: p: { pname = p.pname + "-output";});
+    package = packages.vector;
+    name = "vector-output";
     runtimeScript = ''
       exec ${packages.vector}/bin/vector --config vector-output.toml
     '';
-
-    meta = {
-      description = "A vector instance to parse a std{out,err} from programms";
-    };
+    meta.description = "A vector instance to parse a std{out,err} from programms";
   };
   tmuxinator-infra-minimal = inputs.std.lib.ops.mkOperable {
     name = "tmux-minimal";
